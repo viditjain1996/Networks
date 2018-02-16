6c20c5a76d6c927c89f027e7e66597060d0a48ac
viditjain1996 <viditjain1996@gmail.com>
2018-02-16 12:48:14 +0530

    Practice cont..

---
+++ headers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<errno.h>
+#include<ctype.h>
